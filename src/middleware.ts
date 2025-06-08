27785e57de596c7b34156872f46f293b209da8c5
Lucas Yang <yangchenshin77@gmail.com>
2025-06-08 23:11:23 +0800

    Change to no trailing slash url style

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,12 @@ import { isLoggedIn } from '@/auth'
 export const onRequest = defineMiddleware(async (context, next) => {
   // guest only: 只能由訪客訪問的路徑
   if (['/login/'].includes(context.url.pathname) && await isLoggedIn(context.cookies)) {
-    return context.redirect('/dashboard/')
+    return context.redirect('/dashboard')
   }
 
   // auth only: 只能由已登錄用戶訪問的路徑
   if (['/dashboard/'].includes(context.url.pathname) && !await isLoggedIn(context.cookies)) {
-    return context.redirect('/login/')
+    return context.redirect('/login')
   }
 
   return await next()
